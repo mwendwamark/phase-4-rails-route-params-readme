43c9269524e2727c0ba9f16b086f7fc5adee96eb
mark mwendwa <mwendwamark310@gmail.com>
2023-06-19 12:50:38 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,11 @@
 class CheesesController < ApplicationController
-
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show 
+    show = Cheese.find_by(id: params[:id])
+    render json: show
+  end
 end
